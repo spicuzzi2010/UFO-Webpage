075a565b75944c4014c066f907866a12173acb62
Ryan <ryanspicuzzi@comcast.net>
2021-06-21 17:54:25 -0400

    Completed code for appending the ufo data to a html table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// selecting the tbody element and assigning it to a variable
+var tbody = d3.select("tbody");
+
+//Using a foreach and arrow function to append a row for each ufoReport object
+//Foreach to loop through the tableData
+tableData.forEach((ufoData) => {
+    //Appending a row for each ufoReport object
+    var row = tbody.append("tr");
+    //returning an array of the key,value pairs of ufoData
+    Object.entries(ufoData).forEach(([key, value]) => {
+        //Append a cell for each value in the ufoData object
+        var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
